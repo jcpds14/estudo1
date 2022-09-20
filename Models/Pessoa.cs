b10c8d1ff952d57a79c7d82f775d35b889ca095d
Jean Carlos <jcpds14@gmail.com>
2022-09-20 06:22:38 -0300

    Learning commentaries

--- Models/Pessoa.cs
+++ Models/Pessoa.cs
@@ -5,11 +5,17 @@ using System.Threading.Tasks;
 
 namespace ESTUDO1.Models
 {
+    /// <summary>
+    /// Representa uma pessoa física
+    /// </summary>
     public class Pessoa
     {
         public string? Nome { get; set; }
         public int Idade { get; set; }
 
+        /// <summary>
+        /// Faz a pessoa se apresentar, dizendo seu nome e idade
+        /// </summary>
         public void Apresentar()
         {
             Console.WriteLine($"Olá, me chamo {Nome} \n Tenho {Idade} anos");
